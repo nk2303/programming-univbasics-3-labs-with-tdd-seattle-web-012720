ddaeb6db87b84612eb4f6ec10c47867c65ffe5ba
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-05 03:18:25 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,17 +4,21 @@ first_number = 2
 second_number = 3
 
 def sum()
-  first_number + second_number
+  sum = first_number + second_number
+  return sum
 end
 
 def difference()
-  
+  difference = first_number + second_number
+  return difference
 end
 
 def product()
-  
+  product = first_number + second_number
+  return product
 end
 
 def sum()
-  
+  sum = first_number + second_number
+  return sum
 end
