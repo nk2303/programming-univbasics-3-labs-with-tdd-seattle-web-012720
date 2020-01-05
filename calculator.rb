e76de6511b1cbc8764d890ed3e5604827c4e98ef
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-05 03:30:03 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,28 @@
 # Add your variables here
+
+first_number = 2
+second_number = 3
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
+
+# def sum()
+#   sum = first_number + second_number
+#   return sum
+# end
+
+# def difference()
+#   difference = first_number - second_number
+#   return difference
+# end
+
+# def product()
+#   product = first_number * second_number
+#   return product
+# end
+
+# def quotient()
+#   quotient = first_number / second_number
+#   return quotient
+# end
