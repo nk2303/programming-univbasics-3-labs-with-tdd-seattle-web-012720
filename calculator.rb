92833e93e8f7cc4598c9bc0be811de33879872d4
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-05 03:21:57 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,6 +4,8 @@ first_number = 2
 second_number = 3
 sum = first_number + second_number
 difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
 
 # def sum()
 #   sum = first_number + second_number
